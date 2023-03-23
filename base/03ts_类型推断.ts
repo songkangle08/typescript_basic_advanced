0c561dc7780c96a718a4c5d4f31f90aee7103175
songkangle <songkl@novastar.tech>
2023-03-23 11:14:06 +0800

    类型推断

---
+++ base/03ts_类型推断.ts
@@ -0,0 +1,28 @@
+{
+  let str: string = "this is string";
+  let num: number = 1;
+  let bool: boolean = true;
+}
+{
+  // 在很多情况下，TypeScript 会根据上下文环境自动推断出变量的类型，无须我们再写明类型注解
+  let str = "this is string"; // 等价于let str: string = "this is string";
+  let num = 1; // 等价于let num: number = 1;
+  let bool = true;
+}
+
+{
+  const str: string = "this is string"; // const str: string
+  const num: number = 1;
+  const bool: boolean = true;
+}
+{
+  const str = "this is string"; // const str: "this is string"  不等价于 const str: string = "this is string";
+  const num = 1; // num : 1
+  const bool = true; // bool : true
+}
+
+/*
+  总结：
+  1. 在很多情况下，TypeScript 会根据上下文环境自动推断出变量的类型，无须我们再写明类型注解。
+  2. 在 TypeScript 中，具有初始化值的变量、有默认值的函数参数、函数返回的类型都可以根据上下文推断出来。
+*/
