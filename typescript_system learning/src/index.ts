a5ee3b6b85e329c8e77f80ef980da6ee546ae69c
songkangle <sklhtml@163.com>
2023-08-09 17:50:22 +0800

    ts基础类型

--- typescript_system learning/src/index.ts
+++ typescript_system learning/src/index.ts
@@ -13,8 +13,61 @@ let a: string = "a";
 // 基础类型
 // number、string、boolean、array、tuple、enum、null、undefined、void、never、any、object、symbol、bigint
 
+// number
+let num1: number = 10;
+
+// string
+let str1: string = "string";
+
+// boolean
+let bool1: boolean = false;
+
+// array
+let array1: number[] = [1, 2, 3, 4, 5];
+let array2: Array<number> = [1, 2, 3, 4, 5];
+
+// tuple
+let tuple1: [string, number, string] = ["skl", 18, "西安"];
+
+// enum
+{
+  // 类型可以进行反举 （值是数字的时候 可以反过来枚举）, 枚举没有值会根据上面的索引来自动累加
+  enum Status {
+    no = 0,
+    yes = 1,
+  }
+  // 常量枚举不会额外编译成对象,更节约空间
+  const enum Color {
+    pick,
+    blue,
+    red,
+  }
+  console.log(Color.blue, Color.pick, Color.red);
+  // 异构枚举 就是枚举中不光有数字 还有字符串. 异构枚举上一个是字符串下一个无法推导
+  enum mixE {
+    a,
+    b = "b",
+    c = 1,
+  }
+}
+
+// null和undefined
+// 在严格模式下，null只能赋值给null类型，undefined只能赋值给undefined类型
+// 非严格模式下,null和undefined可以赋值给任何类型(null,undefined是任何类型的子类型)
+let n: null = null;
+let u: undefined = undefined;
+
+// let n1: null = "123";  // 不能将类型“"123"”分配给类型“null”。
+// let str2: string = null; // 非严格模式下不报错
+
+// void 代表空类型，这个void一般只表示函数的返回值
+// undefined可以赋值给void，都
+function fn(): void {}
+
 // symbol
 const symbol: symbol = Symbol("abc");
 
 // bigint
 const bigint: bigint = BigInt(Number.MAX_SAFE_INTEGER + 1);
+
+export {};
